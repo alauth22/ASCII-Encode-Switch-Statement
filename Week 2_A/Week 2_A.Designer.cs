4733bb03b1a4db8c0131edefc3a7ef1289da22d9
25136574 <25136574@STUWIN10-17>
2023-09-06 12:29:09 -0500

    Add project files.

---
+++ Week 2_A/Week 2_A.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Week_2_A
+{
+    partial class frmDecimal_ASCII_Calc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblTypeNumber = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblActualAnswer = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(468, 48);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(132, 31);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // lblTypeNumber
+            // 
+            this.lblTypeNumber.Location = new System.Drawing.Point(28, 41);
+            this.lblTypeNumber.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTypeNumber.Name = "lblTypeNumber";
+            this.lblTypeNumber.Size = new System.Drawing.Size(428, 44);
+            this.lblTypeNumber.TabIndex = 1;
+            this.lblTypeNumber.Text = "Please type a number you want to convert:";
+            this.lblTypeNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.Location = new System.Drawing.Point(348, 85);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(118, 56);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "Answer:";
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActualAnswer
+            // 
+            this.lblActualAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblActualAnswer.Location = new System.Drawing.Point(468, 99);
+            this.lblActualAnswer.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblActualAnswer.Name = "lblActualAnswer";
+            this.lblActualAnswer.Size = new System.Drawing.Size(134, 42);
+            this.lblActualAnswer.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(104, 179);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(150, 71);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(316, 179);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(150, 71);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(526, 179);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(150, 71);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmDecimal_ASCII_Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 298);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblActualAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblTypeNumber);
+            this.Controls.Add(this.txtNumber);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmDecimal_ASCII_Calc";
+            this.Text = "Decimal_ASCII Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblTypeNumber;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblActualAnswer;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Week 2_A/Week 2_A.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_2_A
+{
+    public partial class frmDecimal_ASCII_Calc : Form
+    {
+        public frmDecimal_ASCII_Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //Declare and initialize the variable that will hold the user's input in txtNumber. 
+            int number; 
+
+            //TryParse will check if the value of txtNumber (user input) can be converted to an int. If it can be
+            //converted, TryParse will send the int value to the variable number which we initialized in line 23. 
+            if (int.TryParse(txtNumber.Text, out number))
+            {
+                //Line 30 will check if the value of number is within the correct range, ensuring data integrity. 
+                if (number >= 65 && number <= 90)
+                {
+                    //once value of number has been confirmed as an int and within the range, 
+                    //same value wil be converted to a string for the switch statement. 
+                    string asc_num;
+                    asc_num = txtNumber.Text.ToString();
+
+                    //beginning of switch statement to correlate the number with the respective capital letter. 
+                    switch (asc_num)
+                    {
+                        case "65":
+                            lblActualAnswer.Text = "A";
+                            break;
+                        case "66":
+                            lblActualAnswer.Text = "B";
+                            break;
+                        case "67":
+                            lblActualAnswer.Text = "C";
+                            break;
+                        case "68":
+                            lblActualAnswer.Text = "D";
+                            break;
+                        case "69":
+                            lblActualAnswer.Text = "E";
+                            break;
+                        case "70":
+                            lblActualAnswer.Text = "F";
+                            break;
+                        case "71":
+                            lblActualAnswer.Text = "G";
+                            break;
+                        case "72":
+                            lblActualAnswer.Text = "H";
+                            break;
+                        case "73":
+                            lblActualAnswer.Text = "I";
+                            break;
+                        case "74":
+                            lblActualAnswer.Text = "J";
+                            break;
+                        case "75":
+                            lblActualAnswer.Text = "K";
+                            break;
+                        case "76":
+                            lblActualAnswer.Text = "L";
+                            break;
+                        case "77":
+                            lblActualAnswer.Text = "M";
+                            break;
+                        case "78":
+                            lblActualAnswer.Text = "N";
+                            break;
+                        case "79":
+                            lblActualAnswer.Text = "O";
+                            break;
+                        case "80":
+                            lblActualAnswer.Text = "P";
+                            break;
+                        case "81":
+                            lblActualAnswer.Text = "Q";
+                            break;
+                        case "82":
+                            lblActualAnswer.Text = "R";
+                            break;
+                        case "83":
+                            lblActualAnswer.Text = "S";
+                            break;
+                        case "84":
+                            lblActualAnswer.Text = "T";
+                            break;
+                        case "85":
+                            lblActualAnswer.Text = "U";
+                            break;
+                        case "86":
+                            lblActualAnswer.Text = "V";
+                            break;
+                        case "87":
+                            lblActualAnswer.Text = "W";
+                            break;
+                        case "88":
+                            lblActualAnswer.Text = "X";
+                            break;
+                        case "89":
+                            lblActualAnswer.Text = "Y";
+                            break;
+                        case "90":
+                            lblActualAnswer.Text = "Z";
+                            break;
+                    }
+
+                }
+                else
+                    //Error Message to User if integer is not within the required range. 
+                    MessageBox.Show("Please type an integer value that is within the range of 65 - 90.");
+                txtNumber.Clear();
+                txtNumber.Focus();
+            }
+            else
+                //Error Message to User if invalid data is typed in the textbox on the form. This includes a blank textbox.  
+                MessageBox.Show("Please type ONLY an integer value within the range of 65-90. A non-integer value(s), letter(s), " +
+                                "or a blank are examples of invalid values.");
+            txtNumber.Clear();
+            txtNumber.Focus();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Resets both textboxes. 
+            txtNumber.Text = "";
+            lblActualAnswer.Text = "";
+            txtNumber.Focus();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Closes the form entirely. 
+            this.Close();
+        }
+    }
+}
